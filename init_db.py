531e9d86a1c1b2f9862104d336eac594f513adcb
scipper15 <scipper15@gmx.de>
2023-03-20 21:53:57 +0100

    Added `User` model.
    - Added `drop_all()` in `db_init.py`.
    - Automatic test user creation in `db_init.py`.

--- init_db.py
+++ init_db.py
@@ -1,3 +1,17 @@
-from tablesoccer_rocks.extensions import db
+from werkzeug.security import generate_password_hash
 
+from tablesoccer_rocks.extensions import db
+from tablesoccer_rocks.models.user import User
+
+db.drop_all()
 db.create_all()
+
+# create new default user for testing
+if not db.session.get(User, 1):
+    new_user = User(
+        user_name='info@mitkickzentrale.de',
+        password_hash=generate_password_hash('1234', method='sha256')
+        )
+
+    db.session.add(new_user)
+    db.session.commit()
---
+++ tablesoccer_rocks/models/user.py
@@ -0,0 +1,15 @@
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column
+
+from tablesoccer_rocks.extensions import db
+
+
+class User(db.Model):
+    __tablename__ = "player"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_name: Mapped[str] = mapped_column(String(100), unique=True, nullable=False)
+    password_hash: Mapped[str] = mapped_column(String(100), nullable=False)
+
+
+    def __repr__(self):
+        return f'<User "{self.user_name}">'
