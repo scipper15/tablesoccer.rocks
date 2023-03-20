87ad0487e6abdd7ab5081cff7eec54d06ed65004
scipper15 <scipper15@gmx.de>
2023-03-20 23:05:14 +0100

    Updated `init_db.py` to reflect changes in models.

--- init_db.py
+++ init_db.py
@@ -2,6 +2,7 @@ from werkzeug.security import generate_password_hash
 
 from tablesoccer_rocks.extensions import db
 from tablesoccer_rocks.models.user import User
+from tablesoccer_rocks.models.dyp_models import Player, PlayerHistory, Dyp
 
 db.drop_all()
 db.create_all()
