79098a5bdfffb206cb301660ae637a4ba59964cf
scipper15 <scipper15@gmx.de>
2023-03-30 15:40:24 +0200

    Added entrypoint script for use in docker containers.
    Automatic creation of user in `entrypoint.sh`:
    Change username and password there.
    
    Automates deployment:
    
    - initializes db.
    - creates virtual environment and installs requirements.
    - alters owner and permissions for security reasons.
    - starts gunicorn on port 8000.

---
+++ entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+echo "Initializing database:"
+flask db init
+echo "Creating user:"
+# length >= 8, plus min. 1 symbol, 1 upper and lower case, 1 number
+flask user create new_user new_passworD1@
+
+echo "Creating virtual environment"
+python3 -m venv .venv_linux
+. .venv_linux/bin/activate
+echo "Installing dependencies from requirements.txt"
+pip install -r requirements.txt
+
+echo "Altering owner and permissions"
+chown www-data:www-data -R
+find . -type d -exec chmod 755 {} \;  # folders
+find . -type f -exec chmod 644 {} \;  # files
+
+echo "Starting server for deployment with 4 workers on standard port 8000"
+gunicorn -w 4 'tablesoccer_rocks:create_app()'
